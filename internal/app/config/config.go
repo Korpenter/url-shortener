610bc96f35e39b42aaa1047d70ef013fb90480df
Dmitrii <laksdmitrii@gmail.com>
2023-01-27 20:46:44 +0300

    config from file

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,44 +2,57 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
+	"log"
+	"os"
+	"strings"
 
 	"github.com/kelseyhightower/envconfig"
 )
 
 // Config represents the configuration options for the server.
 type Config struct {
-	ServerAddress string `envconfig:"SERVER_ADDRESS" default:"localhost:8080"`
-	BaseURL       string `envconfig:"BASE_URL" default:"http://localhost:8080"`
-	FileStorage   string `envconfig:"FILE_STORAGE_PATH" default:""`
-	SecretKey     []byte `envconfig:"URL_SHORTENER_KEY" default:"defaultKeyUrlSHoRtenEr"`
-	PostgresURL   string `envconfig:"DATABASE_DSN" default:""`
-	EnableHTTPS   bool   `envconfig:"ENABLE_HTTPS" default:"false"`
-	CertFile      string `envconfig:"TLS_CERT_FILE" default:"cert.pem"`
-	KeyFile       string `envconfig:"TLS_KEY_FILE" default:"key.pem"`
+	ServerAddress string `envconfig:"SERVER_ADDRESS" default:"localhost:8080" json:"server_address"`
+	BaseURL       string `envconfig:"BASE_URL" default:"http://localhost:8080" json:"base_url"`
+	FileStorage   string `envconfig:"FILE_STORAGE_PATH" default:"" json:"file_storage_path"`
+	SecretKey     []byte `envconfig:"URL_SHORTENER_KEY" default:"defaultKeyUrlSHoRtenEr" json:"secret_key"`
+	PostgresURL   string `envconfig:"DATABASE_DSN" default:"" json:"database_dsn"`
+	EnableHTTPS   bool   `envconfig:"ENABLE_HTTPS" default:"false" json:"enable_https"`
+	CertFile      string `envconfig:"TLS_CERT_FILE" default:"cert.pem" json:"cert_file"`
+	KeyFile       string `envconfig:"TLS_KEY_FILE" default:"key.pem" json:"key_file"`
 }
 
 // NewConfig initializes and returns a new Config struct. It reads
 // environment variables and command-line flags to set the configuration values.
 func NewConfig() *Config {
 	var c Config
+	configFile := os.Getenv("CONFIG")
 	envconfig.MustProcess("", &c)
 	flag.StringVar(&c.ServerAddress, "a", c.ServerAddress, "server address")
 	flag.StringVar(&c.BaseURL, "b", c.BaseURL, "base url address")
 	flag.StringVar(&c.FileStorage, "f", c.FileStorage, "storage path")
 	flag.StringVar(&c.PostgresURL, "d", c.PostgresURL, "postgres url")
 	flag.BoolVar(&c.EnableHTTPS, "s", c.EnableHTTPS, "enable https")
-	flag.StringVar(&c.CertFile, "c", c.CertFile, "tls cert file path")
+	flag.StringVar(&c.CertFile, "l", c.CertFile, "tls cert file path")
 	flag.StringVar(&c.KeyFile, "t", c.KeyFile, "tls key file path")
+	flag.StringVar(&configFile, "c", configFile, "path to config file")
 	key := flag.String("k", "", "key")
 	flag.Parse()
+	if configFile != "" {
+		bytes, err := os.ReadFile(configFile)
+		if err != nil {
+			log.Fatalf("failed to read cfg from file: %v", err)
+		}
 
-	if c.EnableHTTPS {
-		c.BaseURL = "https://" + c.ServerAddress
-	} else {
-		c.BaseURL = "http://" + c.ServerAddress
+		err = json.Unmarshal(bytes, &c)
+		if err != nil {
+			log.Fatalf("failed to decode cfg from file: %v", err)
+		}
 	}
-
+	if c.EnableHTTPS {
+		strings.Replace(c.BaseURL, "http://", "https://", 1)
+	}
 	if *key != "" {
 		c.SecretKey = []byte(*key)
 	}
